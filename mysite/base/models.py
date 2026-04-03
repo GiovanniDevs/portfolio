9ad6560c7039ce14c71ea6416f861a9bfc15669d
GiovanniDevs <damico.giovanni.90@gmail.com>
2026-04-03 12:00:52 +0100

    add form and reusable streamfield blocks

--- mysite/base/models.py
+++ mysite/base/models.py
@@ -1,9 +1,10 @@
 from django.db import models
-
-# Create your models here.
+from modelcluster.fields import ParentalKey
 
 from wagtail.admin.panels import (
     FieldPanel,
+    FieldRowPanel,
+    InlinePanel,
     MultiFieldPanel,
     PublishingPanel,
 )
@@ -24,6 +25,9 @@ from wagtail.contrib.settings.models import (
     BaseGenericSetting,
     register_setting,
 )
+
+from wagtail.contrib.forms.models import AbstractEmailForm, AbstractFormField
+from wagtail.contrib.forms.panels import FormSubmissionsPanel
 
 
 @register_setting
@@ -68,3 +72,27 @@ class FooterText(
 
     class Meta(TranslatableMixin.Meta):
         verbose_name_plural = "Footer Text"
+
+
+class FormField(AbstractFormField):
+    page = ParentalKey('FormPage', on_delete=models.CASCADE,
+                       related_name='form_fields')
+
+
+class FormPage(AbstractEmailForm):
+    intro = RichTextField(blank=True)
+    thank_you_text = RichTextField(blank=True)
+
+    content_panels = AbstractEmailForm.content_panels + [
+        FormSubmissionsPanel(),
+        FieldPanel('intro'),
+        InlinePanel('form_fields'),
+        FieldPanel('thank_you_text'),
+        MultiFieldPanel([
+            FieldRowPanel([
+                FieldPanel('from_address'),
+                FieldPanel('to_address'),
+            ]),
+            FieldPanel('subject'),
+        ], "Email"),
+    ]
