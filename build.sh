3b869870374e31105f0a555bd7d20ea82c2e7823
GiovanniDevs <damico.giovanni.90@gmail.com>
2026-04-06 11:37:21 +0100

    clear build.sh from echo logs

--- build.sh
+++ build.sh
@@ -3,10 +3,5 @@
 set -o errexit
 cd mysite
 pip install -r requirements.txt
-
-echo "=== DJANGO_SETTINGS_MODULE: $DJANGO_SETTINGS_MODULE ==="
-echo "=== Current directory: $(pwd) ==="
-python -c "import django; django.setup(); from django.conf import settings; print('STATIC_ROOT:', settings.STATIC_ROOT); print('STATICFILES_DIRS:', settings.STATICFILES_DIRS); print('INSTALLED_APPS:', settings.INSTALLED_APPS)"
-
-python manage.py collectstatic --no-input --clear -v 2
+python manage.py collectstatic --no-input --clear
 python manage.py migrate
