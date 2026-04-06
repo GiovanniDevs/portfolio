eab15dd0c4d63932c2f39d858f25ab2b2db6c3aa
GiovanniDevs <damico.giovanni.90@gmail.com>
2026-04-06 11:16:01 +0100

    try static fix

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ pip install -r requirements.txt
 
 # Collect static files (CSS, JS, images) into one folder
 # WhiteNoise serves them from there
-python manage.py collectstatic --no-input
+python manage.py collectstatic --no-input --clear
 
 # Run database migrations
 python manage.py migrate
