ac79c8cf12cbbe83666726e5d4ef5876ff119c30
GiovanniDevs <damico.giovanni.90@gmail.com>
2026-04-06 11:21:58 +0100

    debugging_01

--- build.sh
+++ build.sh
@@ -1,16 +1,12 @@
 #!/usr/bin/env bash
 # Exit on error
 set -o errexit
-
 cd mysite
-
-# Install Python dependencies
 pip install -r requirements.txt
 
+echo "=== DJANGO_SETTINGS_MODULE: $DJANGO_SETTINGS_MODULE ==="
+echo "=== Current directory: $(pwd) ==="
+python -c "import django; django.setup(); from django.conf import settings; print('STATIC_ROOT:', settings.STATIC_ROOT); print('STATICFILES_DIRS:', settings.STATICFILES_DIRS); print('INSTALLED_APPS:', settings.INSTALLED_APPS)"
 
-# Collect static files (CSS, JS, images) into one folder
-# WhiteNoise serves them from there
-python manage.py collectstatic --no-input --clear
-
-# Run database migrations
+python manage.py collectstatic --no-input --clear -v 2
 python manage.py migrate
