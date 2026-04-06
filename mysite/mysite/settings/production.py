096c11d276b68447b2c26dec53907f032c8426fc
GiovanniDevs <damico.giovanni.90@gmail.com>
2026-04-06 09:56:17 +0100

    switch to the less strict backend

--- mysite/mysite/settings/production.py
+++ mysite/mysite/settings/production.py
@@ -31,7 +31,7 @@ STORAGES = {
         "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     },
 }
 
