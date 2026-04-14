d57023bd6196ba4d417065980b16400d860b0cb8
GiovanniDevs <damico.giovanni.90@gmail.com>
2026-04-14 12:37:03 +0100

    remove hardcoded top porject and adds it to the model

--- mysite/home/models.py
+++ mysite/home/models.py
@@ -42,11 +42,29 @@ class HomePage(Page):
     intro = RichTextField(blank=True)
     about = RichTextField(blank=True)
 
-    featured_posts = StreamField(
-        [("post", PageChooserBlock(page_type="blog.BlogPage"))],
+    top_project_title = models.CharField(
+        max_length=120,
         blank=True,
-        use_json_field=True,
-        help_text="Pivck blog posts to feature on the homepage.",
+        help_text="Main title for the top project card"
+    )
+
+    top_project_text = models.TextField(
+        blank=True,
+        help_text="Short project description"
+    )
+
+    top_project_stack = models.TextField(
+        blank=True,
+        help_text="One technology per line, for example: Python then Django then PostgreSQL"
+    )
+
+    top_project_image = models.ForeignKey(
+        "wagtailimages.Image",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name="+",
+        help_text="Top project image"
     )
 
     featured_projects = StreamField(
@@ -56,6 +74,13 @@ class HomePage(Page):
         help_text="Use this section to showcase your projects"
     )
 
+    featured_posts = StreamField(
+        [("post", PageChooserBlock(page_type="blog.BlogPage"))],
+        blank=True,
+        use_json_field=True,
+        help_text="Pivck blog posts to feature on the homepage.",
+    )
+
     def get_context(self, request):
         context = super().get_context(request)
         latest = BlogPage.objects.live().order_by('-first_published_at').first()
@@ -63,6 +88,9 @@ class HomePage(Page):
         return context
 
     content_panels = Page.content_panels + [
+
+
+
         MultiFieldPanel(
             [
                 FieldPanel("image"),
@@ -72,9 +100,25 @@ class HomePage(Page):
             ],
             heading="Hero section",
         ),
+
         FieldPanel('intro'),
-        FieldPanel("featured_posts"),
-        FieldPanel("featured_projects"),
         FieldPanel('about'),
+
+        MultiFieldPanel(
+            [
+                FieldPanel("top_project_title"),
+                FieldPanel("top_project_text"),
+                FieldPanel("top_project_stack"),
+                FieldPanel("top_project_image"),
+            ],
+            heading="Top project section",
+        ),
+
+        FieldPanel("featured_projects"),
+
+
+        FieldPanel("featured_posts"),
+
+
 
     ]
